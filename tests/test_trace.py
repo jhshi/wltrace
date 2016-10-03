e44ba3ed9486f258c74ad44156136fbad3d7bfb4
Jinghao Shi <jinghaos@buffalo.edu>
2016-10-03 18:04:12 -0400

    working

--- tests/test_trace.py
+++ tests/test_trace.py
@@ -17,21 +17,33 @@ def test_pcap():
     assert len(pkts) == 318
 
     pkt = pkts[0]
+    assert pkt.counter == 1
 
     # PHY
-    assert pkt.counter == 1
-    assert pkt.phy.mactime == 84523414517
+    assert pkt.phy.signal == -47
+    assert pkt.phy.noise is None
+    assert pkt.phy.freq_mhz == 5200
     assert pkt.phy.has_fcs
     assert not pkt.phy.fcs_error
-    assert pkt.phy.rate == 6
-    assert pkt.phy.freq_mhz == 5200
-    assert abs(pkt.epoch_ts - 1474410869.121930000) < 1e-6
     assert pkt.phy.len == 117
     assert pkt.phy.caplen == 117
+    assert pkt.phy.rate == 6
+    assert pkt.phy.mcs is None
+    assert pkt.phy.mactime == 84523414517
+    assert abs(pkt.phy.epoch_ts - 1474410869.121930000) < 1e-6
+    assert (pkt.phy.end_epoch_ts - pkt.phy.epoch_ts)*6e6/8 == pkt.phy.len
 
     # MAC
     assert pkt.type == dot11.DOT11_TYPE_MANAGEMENT
     assert pkt.subtype == dot11.DOT11_SUBTYPE_BEACON
+    assert not pkt.from_ds
+    assert not pkt.to_ds
+    assert not pkt.more_frag
+    assert not pkt.rety
+    assert not pkt.power
+    assert not pkt.more_data
+    assert not pkt.protected
+    assert not pkt.order
     assert pkt.duration == 0
     assert dot11.is_broadcast(pkt.dest)
     assert pkt.src == '10:fe:ed:e5:8c:97'
